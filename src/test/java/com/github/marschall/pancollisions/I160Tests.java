cbbdb1c3cdbe64c4326cded176afead41a6f22b9
Philippe Marschall <philippe.marschall@gmail.com>
2020-07-25 14:22:52 +0200

    fix tests

--- src/test/java/com/github/marschall/pancollisions/I160Tests.java
+++ src/test/java/com/github/marschall/pancollisions/I160Tests.java
@@ -43,7 +43,7 @@ class I160Tests {
         (byte) 0b10101010,
     });
 
-    assertEquals(0b1010101011111111000000001111111, i160.getFirstBits(31));
+    assertEquals(0b10101010111111110000000011111110, i160.getFirstBits());
   }
 
   @Test
@@ -74,63 +74,7 @@ class I160Tests {
         (byte) 0b10101010,
         (byte) 0b10101010,
     });
-    assertEquals(0, i160.getNumberOfLeadingZeroes(31));
-
-    i160 = I160.valueOf(new byte[] {
-        (byte) 0b10101010,
-        (byte) 0b11111111,
-        (byte) 0b00000000,
-        (byte) 0b11111110,
-
-        (byte) 0b01101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-        (byte) 0b10101010,
-    });
-    assertEquals(2, i160.getNumberOfLeadingZeroes(31));
-
-    i160 = I160.valueOf(new byte[] {
-        (byte) 0b10101010,
-        (byte) 0b11111111,
-        (byte) 0b00000000,
-        (byte) 0b11111110,
-
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000010,
-
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-    });
-    assertEquals(2, i160.getNumberOfLeadingZeroes(31));
+    assertEquals(0, i160.getNumberOfLeadingZeroes());
 
     i160 = I160.valueOf(new byte[] {
         (byte) 0b10101010,
@@ -148,17 +92,17 @@ class I160Tests {
         (byte) 0b00000000,
         (byte) 0b00000000,
 
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
+        (byte) 0b00101010,
+        (byte) 0b10101010,
+        (byte) 0b10101010,
+        (byte) 0b10101010,
 
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000000,
-        (byte) 0b00000010,
+        (byte) 0b10101010,
+        (byte) 0b10101010,
+        (byte) 0b10101010,
+        (byte) 0b10101010,
     });
-    assertEquals(2, i160.getNumberOfLeadingZeroes(63));
+    assertEquals(66, i160.getNumberOfLeadingZeroes());
   }
 
 }
